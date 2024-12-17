2e518d17eb477b122dcd1da824800574be61d4ae
sadt <github@sadt.me>
2024-12-17 18:52:32 +0700

    fix volar

--- lua/ide/lsp/servers/volar.lua
+++ lua/ide/lsp/servers/volar.lua
@@ -1,8 +1,17 @@
 return {
 	init_options = {
+		typescript = {
+			tsdk = vim.fn.stdpath('data') .. "/mason/packages/typescript-language-server/node_modules/typescript/lib"
+		},
 		vue = {
-			hybridMode = true
+			hybridMode = false
 		}
 	},
+	on_new_config = function(new_config, new_root_dir)
+		local lib_path = vim.fs.find('node_modules/typescript/lib', { path = new_root_dir, upward = true })[1]
+		if lib_path then
+			new_config.init_options.typescript.tsdk = lib_path
+		end
+	end,
 	filetypes = { 'vue' }
 }
